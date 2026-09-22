9bd2b7e6971d13282ccadfca79c34c1c012f7383
gercg-926 <germancg926@gmail.com>
2026-09-22 14:18:15 +0200

    ft_strtrim

--- Milestone_0/Libft/ft_strtrim.c
+++ Milestone_0/Libft/ft_strtrim.c
@@ -16,7 +16,8 @@ int	ft_isinstr(char c, char const *set)
 {
 	int	i;
 
-	while(set[i] != '/0')
+	i = 0;
+	while(set[i] != '\0')
 	{
 		if (set[i] == c)
 			return (1);
@@ -29,15 +30,49 @@ char*	ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
 	int		i;
-	int		j;
+	int		end;
 	int		start;
 
+	if (!s1 || !set)
+        return (NULL);
 	i = 0;
-	while (s1[i] != '/0')
+	start = 0;
+	while (s1[start] && ft_isinstr(s1[start], set))
+        start++;
+	if (s1[start] == '\0')
+    {
+        str = malloc(1);
+        if (str)
+            str[0] = '\0';
+        return (str);
+    }
+	end = ft_strlen(s1) - 1;
+	while (end >= start && ft_isinstr(s1[end], set))
+        end--;
+	str = malloc(sizeof (char) * (end - start) + 2);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start < end + 1)
 	{
-		if (ft_isinstr(s1[i], set))
-			i++;
-		
+		str[i] = s1[start];
+		i++;
+		start++;
 	}
-	
+	str[i] = '\0';
+	return (str);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    char const *str = "-++-hola+mundo+---++-";
+    char const *set = "+-";
+    
+	char *res = ft_strtrim(str, set);
+    printf("Resultado: '%s'\n", res);
+    
+    free(res); // Recuerda liberar la memoria reservada con malloc
+    return 0;
 }
